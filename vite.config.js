dcb3136da01f00644d7d07a8aab078a472755233
eilx <eil23@pm.me>
2022-07-10 12:17:08 +0100

    Setup CSS (nesting)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/memory-game/'
+  base: '/memory-game/',
+
+  css: {
+    postcss: {
+      plugins: [ postcssNesting ]
+    }
+  }
 })
